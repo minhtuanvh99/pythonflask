5e45d6181aeb9a234958ec721f0d0406425a6ce0
MinhTuan <minhtuanvh99@gmail.com>
2020-12-09 23:34:02 +0700

    create database using SQL-Alchemy

--- myblog.py
+++ myblog.py
@@ -1,1 +1,10 @@
-from server import app
+from server import app, db
+from server.models import User, Post
+
+
+# working with flash shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from config import Config
+# use database with local uri
+from flask_sqlalchemy import SQLAlchemy
+# tranverse from local database to another database
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+# set database config
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # get all configuration of the app from config module 
 app.config.from_object(Config)
 
 
-from server import routes
+from server import routes, models
