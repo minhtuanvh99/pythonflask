048cf6f37b29c5a566eef6c2f2e51b4036f147eb
MinhTuan <minhtuanvh99@gmail.com>
2020-12-10 19:52:30 +0700

    error handling, setting up email config

--- server/__init__.py
+++ server/__init__.py
@@ -6,6 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 # provide user session management, handle common tasks of login, logout, remember user session
 from flask_login import LoginManager
+#
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 
@@ -23,5 +27,36 @@ login.login_view = 'login'
 # get all configuration of the app from config module 
 app.config.from_object(Config)
 
+# 
+if not app.debug:
+    # config mail
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Myblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    # write a log file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/myblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+ 
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Myblog startup')
 
-from server import routes, models
+
+
+from server import routes, models, errors
